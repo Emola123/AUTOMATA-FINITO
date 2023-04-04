cbc17ad304b82eb818e0fbee7736f1e09e72d2fa
Privado11 <wjimenez@unimagdalena.edu.co>
2023-04-03 21:52:20 -0500

    Idiomas

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const containerLoader = document.getElementById('load');
+const loaderCircle = document.getElementById('circle');
+const textLoader = document.getElementById('textLoad');
+
+const activarForm = document.getElementById('insertarPalabra')
+const cuboIcon = document.getElementById('cuboIcon')
+const form = document.getElementById('form')
+
+window.addEventListener('load',()=>{
+    setTimeout(()=>{
+        containerLoader.classList.remove('load');
+        containerLoader.classList.add('oculto');
+    },2000)
+})
+
+activarForm.addEventListener('click',()=>{
+    cuboIcon.classList.add('oculto')
+    form.classList.remove('oculto')
+})
