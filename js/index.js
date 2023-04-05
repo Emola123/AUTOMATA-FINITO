037e561fef2e9ad8c4bb65bff950cebfd2c31bd1
desingkuro <andresmauricio745@gmail.com>
2023-04-04 19:08:12 -0500

    estado correcto

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ const activarForm = document.getElementById('insertarPalabra')
 const cuboIcon = document.getElementById('cuboIcon')
 const form = document.getElementById('form')
 
+const activarHistorial = document.getElementById('historial')
+
 window.addEventListener('load',()=>{
     setTimeout(()=>{
         containerLoader.classList.remove('load');
@@ -17,3 +19,9 @@ activarForm.addEventListener('click',()=>{
     cuboIcon.classList.add('oculto')
     form.classList.remove('oculto')
 })
+
+activarHistorial.addEventListener('click',()=>{
+    cuboIcon.classList.add('oculto')
+    form.classList.add('oculto')
+})
+
